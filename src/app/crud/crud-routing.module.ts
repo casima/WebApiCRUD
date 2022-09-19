c373ca57091b127ad4472bbac32e67f7c8082115
casima <brunoabcx@gmail.com>
2022-09-18 22:32:23 -0300

    Configurando rotas para  produtos

--- src/app/crud/crud-routing.module.ts
+++ src/app/crud/crud-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductListComponent} from "./product-list/product-list.component";
+import {ProductFormComponent} from "./product-form/product-form.component";
+import {ProductDetailsComponent} from "./product-details/product-details.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'prod-list', pathMatch: 'full'},
+  {path: 'prod-list', component: ProductListComponent},
+  {path: 'prod-create', component: ProductFormComponent},
+  {path: 'prod-update/:productId', component: ProductFormComponent},
+  {path: 'prod-detail/:productId', component: ProductDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
